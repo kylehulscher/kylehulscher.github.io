7ef5731f3c510fb827cf5943581e1cf86ea36e98
Kyle <hulscher@uiowa.edu>
2022-09-08 21:51:05 -0500

    working on loading word list, pushing so that it can be fetched from github

--- tbt.js
+++ tbt.js
@@ -31,3 +31,20 @@ document.addEventListener('keydown', function(event) {
 		}
 	}
 });
+
+// 👇️ if using ES6 Imports uncomment line below
+// import {readFileSync, promises as fsPromises} from 'fs';
+const {readFileSync, promises: fsPromises} = require('fs');
+
+// ✅ read file SYNCHRONOUSLY
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+  return arr;
+}
+
+syncReadFile('./word_list.csv');
