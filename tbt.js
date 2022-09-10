e09baa4f94e0d121ea0cb9a0d8b0d553f421dc14
Kyle <hulscher@uiowa.edu>
2022-09-10 00:48:52 -0500

    text input refined, error highlighting in progress. Will change format from prompt box and text box to just prompt box with completion and error highlighting in the future after highlighting functionality is more complete.

--- tbt.js
+++ tbt.js
@@ -1,36 +1,58 @@
 // trial by typing js1
+
+const myRequest = new Request('https://raw.githubusercontent.com/kylehulscher/kylehulscher.github.io/main/word_list.json');
+var myList = [];
+
+function loadWordList() {
+	fetch(myRequest)
+	  .then((response) => response.json())
+	  .then((data) => {
+	    for(var i = 0; i < data.length; i++) {
+	    	myList.push(data[i].of);
+	    }}).then(function() {
+			for (var i = 0; i < 40; i++) {
+				var ind = Math.floor(Math.random() * myList.length);
+				document.getElementById('content_header1').innerHTML += myList[ind] + " ";
+			}})
+		}
+
+loadWordList();
+
+function headingComparator() {
+	var header1html = document.getElementById('content_header1').innerHTML + "";
+	var header2html = document.getElementById('content_header2').innerHTML + "";
+	for (var i = 0; i < header2html.length; i++) {
+		if (header2html[i] != header1html[i]) {
+			console.log("no");
+			document.getElementById('content_header2').innerHTML = header2html.substring(0, i) + '<span class="wrongChar">' + header2html[i] + '</span>' + header2html.substring(i + 1, header2html.length);
+			break;
+		}
+	}
+}
+
 document.addEventListener('keydown', function(event) {
-	var newContent = document.getElementById('content_header1').innerHTML + "";
+	var header1String = document.getElementById('content_header1').innerText + "";
+	var header2String = document.getElementById('content_header2').innerText + "";
 	if (event.key == 'Backspace') {
-		newContent = document.getElementById('content_header1').innerHTML + "";
-		if (newContent[newContent.length - 1] == ";") {
-			for (let i = newContent.length - 1; i >= 0; i--) {
-				if (newContent[i] == "&") {
-					document.getElementById('content_header1').innerHTML = newContent.substring(0, i);
+		if (header2String[header2String.length - 1] == ";") {
+			for (let i = header2String.length - 1; i >= 0; i--) {
+				if (header2String[i] == "&") {
+					document.getElementById('content_header2').innerText = header2String.substring(0, i);
 					break;
 				}
 				else if (i == 0) {
-					document.getElementById('content_header1').innerHTML = newContent.substring(0, newContent.length - 1);
+					document.getElementById('content_header2').innerText = header2String.substring(0, header2String.length - 1);
 				}
 			}
 		}
 		else {
-			document.getElementById('content_header1').innerHTML = newContent.substring(0, newContent.length - 1);
+			document.getElementById('content_header2').innerText = header2String.substring(0, header2String.length - 1);
 		}
 	}
 	else if (event.keyCode == 32 || event.keyCode >= 48 && event.keyCode <= 90 || event.keyCode >= 96 && event.keyCode <= 111 || event.keyCode >= 186 && event.keyCode <= 222) {
-		if (newContent.length < 500) {
-			if (event.keyCode == 32) {
-				document.getElementById('content_header1').innerHTML += "&nbsp;";
-			}
-			else {
-				document.getElementById('content_header1').innerHTML += event.key;
-			}
+		if (header2String.length < header1String.length - 1) {
+			document.getElementById('content_header2').innerText += event.key;
 		}
 	}
+	headingComparator();
 });
-
-
-fetch('https://raw.githubusercontent.com/kylehulscher/kylehulscher.github.io/main/word_list.csv')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
